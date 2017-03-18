889612ffd1e4362a891e52a2e415c75d8d3a9f9c
Igor Sakelari <sakelari.igor@gmail.com>
2017-03-17 22:03:00 -0400

    Compressed images using grunt and customized cover image

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,34 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
+    var mozjpeg = require('imagemin-mozjpeg');
+
     grunt.initConfig({
 		concat_css: {
 			dist: {
                 src: ["css/style.css" , "css/responsive.css"],
                 dest: "dist/css/styles.css"
 			}
-		}
+		},
+        imagemin: {                          // Task
+          static: {                          // Target
+            options: {                       // Target options
+              optimizationLevel: 3,
+              svgoPlugins: [{ removeViewBox: false }],
+              use: [mozjpeg()]
+            },
+            files: {                         // Dictionary of files
+              'dist/animal-card-small.jpg': 'images/animal-card.jpg',
+              'dist/ice-cream-recipe-small.jpg': 'images/ice-cream-recipe.jpg',
+              'dist/flipboard-small.jpg': 'images/flipboard.jpg',
+              'dist/cover-small.jpg' : 'images/cover.jpg' // 'destination': 'source'
+            }
+          }
+        }
 	});
 
     grunt.registerTask('default', [
-		'concat_css'
+		'concat_css',
+        'imagemin'
 	]);
 };
